e63c92f225aed21a49e12a43ebeab1beddda1678
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-12 00:06:35 +0900

    created email confirmation function with device

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  # mail setting
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => ENV['SMTP_USER_NAME'],
+  :password => ENV['SMTP_PASSWD'],
+  :authentication => 'plain',
+  :enable_starttls_auto => true,
+}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -33,10 +47,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
