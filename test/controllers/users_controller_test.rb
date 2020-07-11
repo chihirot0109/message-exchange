f21f06198963545d0214f68728f922d8829ddc22
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-11 21:36:27 +0900

    tested index view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @users = Users.all
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,14 @@
-class ActiveSupport::TestCase
-  include Warden::Test::Helpers
-  Warden.test_mode!
-end
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  include Devise::Test::IntegrationHelpers
+
 end
-Capybara.save_path = Rails.root.join('tmp/capybara')
-Capybara.javascript_driver = :headless_chrome
