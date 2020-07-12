b51e24c1a6000e3de34a57232e29a5275e11fa49
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-12 23:30:24 +0900

    added dependent destroy to message and conversation models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,5 @@
 class Conversation < ApplicationRecord
   belongs_to :starter, foreign_key: :starter_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save :capitalize_name
   before_update :capitalize_name
 
-  has_many :messages
+  has_many :conversations, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
