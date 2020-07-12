67ff96a5dffd8c7e04cb60b90ad79780d42923c5
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-12 13:34:42 +0900

    updated User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  before_save :capitalize_name
+  before_update :capitalize_name
+
   has_many :messages
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  def capitalize_name
+    self.first_name = first_name.capitalize
+    self.last_name = last_name.capitalize
+  end
 end
