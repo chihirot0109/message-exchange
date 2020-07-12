731719980ec6d10c34eaf02cc57b8e7ba2f64cff
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-12 15:28:07 +0900

    organized navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
-  resources :messages
+  resources :conversation, only: [:create] do
+    resources :messages
+  end
 end
