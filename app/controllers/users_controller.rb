ba8d37c328244acaaf2abb03fc86c1e5dc34b212
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-13 21:51:58 +0900

    paginated by items by distinguishing class name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def index
     @conversations = current_user.conversations.joins(:messages).order('messages.created_at DESC').uniq
     @users = User.where.not(id: @conversations.pluck(:starter_id, :recipient_id).flatten.uniq)
+    @users = @users.where.not('id = ?', current_user.id)
     @list = @conversations + @users
     @list = Kaminari.paginate_array(@list).page(params[:page]).per(20)
     @new_conversation = Conversation.new
